c510a9281540fc98f494f2dd9a0ff7be4a8e05ac
geliusheng1 <geliusheng1@onemt.com.cn>
2024-09-02 03:53:11 +0000

    简单贪心算法

--- leetcode/test9.go
+++ leetcode/test9.go
@@ -2,7 +2,9 @@
 // 假设1元、2元、5元、10元、20元、50元、100元的纸币分别有c0, c1, c2, c3, c4, c5, c6张。现在要用这些钱来支付K元，至少要用多少张纸币？
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 type Money struct {
 	val, cnt int
@@ -11,20 +13,21 @@ type Money struct {
 func main() {
 	fmt.Println("== main begin ==")
 	money := []Money{{1, 20}, {2, 20}, {5, 20}, {10, 20}, {50, 20}, {100, 20}}
+	var result []Money
 	pay := 532
-	amount := 0
-	for _, v := range money {
-		amount += v.val * v.cnt
+	for i := len(money) - 1; i >= 0; i-- {
+		cel := money[i]
+		cnt_ := min(cel.cnt, pay/cel.val)
+		if cnt_ > 0 {
+			pay -= cnt_ * cel.val
+			result = append(result, Money{cel.val, cnt_})
+		}
 	}
-	if pay > amount {
-		fmt.Println("pay > amount", pay, amount)
-		return
+	if pay == 0 {
+		fmt.Println("find ok result=", result)
+	} else {
+		fmt.Println("find fail result=", result)
 	}
-	tmppay := pay
-	for tmppay > 0 {
-
-	}
-
 	fmt.Println("== main end ==")
 }
 
