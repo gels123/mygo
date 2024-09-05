c423fd5291956579f4095a57fb497274184ee45a
geliusheng1 <geliusheng1@onemt.com.cn>
2024-09-05 02:03:06 +0000

    my practice in go language

--- leetcode/test9.go
+++ leetcode/test9.go
@@ -2,34 +2,30 @@
 // 假设1元、2元、5元、10元、20元、50元、100元的纸币分别有c0, c1, c2, c3, c4, c5, c6张。现在要用这些钱来支付K元，至少要用多少张纸币？
 package main
 
-import (
-	"fmt"
-)
-
 type Money struct {
 	val, cnt int
 }
 
-func main() {
-	fmt.Println("== main begin ==")
-	money := []Money{{1, 20}, {2, 20}, {5, 20}, {10, 20}, {50, 20}, {100, 20}}
-	var result []Money
-	pay := 532
-	for i := len(money) - 1; i >= 0; i-- {
-		cel := money[i]
-		cnt_ := min(cel.cnt, pay/cel.val)
-		if cnt_ > 0 {
-			pay -= cnt_ * cel.val
-			result = append(result, Money{cel.val, cnt_})
-		}
-	}
-	if pay == 0 {
-		fmt.Println("find ok result=", result)
-	} else {
-		fmt.Println("find fail result=", result)
-	}
-	fmt.Println("== main end ==")
-}
+// func main() {
+// 	fmt.Println("== main begin ==")
+// 	money := []Money{{1, 20}, {2, 20}, {5, 20}, {10, 20}, {50, 20}, {100, 20}}
+// 	var result []Money
+// 	pay := 532
+// 	for i := len(money) - 1; i >= 0; i-- {
+// 		cel := money[i]
+// 		cnt_ := min(cel.cnt, pay/cel.val)
+// 		if cnt_ > 0 {
+// 			pay -= cnt_ * cel.val
+// 			result = append(result, Money{cel.val, cnt_})
+// 		}
+// 	}
+// 	if pay == 0 {
+// 		fmt.Println("find ok result=", result)
+// 	} else {
+// 		fmt.Println("find fail result=", result)
+// 	}
+// 	fmt.Println("== main end ==")
+// }
 
 func test_merge() {
 
